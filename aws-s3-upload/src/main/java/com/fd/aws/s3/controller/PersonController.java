877d81a4b93ddaf529edec078954048f42e6a7d9
Furkan Kürşat Danışmaz <danismaz.furkan@gmail.com>
2018-12-19 16:38:09 +0300

    REST API with S3 Backend completed
    POST, GET, GET ALL, and DELETE endpoints are created with S3 backend

--- aws-s3-upload/src/main/java/com/fd/aws/s3/controller/PersonController.java
+++ aws-s3-upload/src/main/java/com/fd/aws/s3/controller/PersonController.java
@@ -1,7 +1,7 @@
 package com.fd.aws.s3.controller;
 
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.CreateBucketRequest;
+import com.amazonaws.services.s3.model.*;
 import com.fd.aws.s3.model.Person;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +11,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,18 +31,33 @@ public class PersonController {
     @Value("${aws.s3.bucket}")
     private String awsS3Bucket;
 
+    private static final String OBJECT_UPLOAD_FOLDER = "upload/object/";
+
     @GetMapping("/{id}")
     public ResponseEntity<Person> get(@PathVariable int id) {
-        Person p = Person.builder().name("furkan").surname("danismaz").build();
-        return new ResponseEntity<Person>(p, HttpStatus.OK);
+        try {
+            S3Object object = this.s3client.getObject(new GetObjectRequest(this.awsS3Bucket, OBJECT_UPLOAD_FOLDER + id));
+            Person p = Person.fromJson(object.getObjectContent());
+            return new ResponseEntity<Person>(p, HttpStatus.OK);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return null;
+        }
     }
 
     @GetMapping
-    public ResponseEntity<List<Person>> getAll() {
-        Person p = Person.builder().name("furkan").surname("danismaz").build();
-        List<Person> all = new ArrayList<Person>();
-        all.add(p);
-        return new ResponseEntity<List<Person>>(all, HttpStatus.OK);
+    public ResponseEntity<List<Person>> getAll() throws IOException {
+        ObjectListing objectListing = this.s3client.listObjects(new ListObjectsRequest()
+                        .withBucketName(this.awsS3Bucket)
+                        .withPrefix(OBJECT_UPLOAD_FOLDER));
+        List<Person> result = new ArrayList<Person>();
+        for (S3ObjectSummary objectSummary : objectListing.getObjectSummaries()) {
+            String key = objectSummary.getKey();
+            S3Object object = this.s3client.getObject(new GetObjectRequest(this.awsS3Bucket, key));
+            Person p = Person.fromJson(object.getObjectContent());
+            result.add(p);
+        }
+        return new ResponseEntity<List<Person>>(result, HttpStatus.OK);
     }
 
     @PostMapping
@@ -49,11 +66,20 @@ public class PersonController {
             if (!this.s3client.doesBucketExistV2(this.awsS3Bucket)) {
                 this.s3client.createBucket(new CreateBucketRequest(this.awsS3Bucket));
             }
-            this.s3client.putObject(this.awsS3Bucket, person.getName(), person.toString());
+            this.s3client.putObject(this.awsS3Bucket, OBJECT_UPLOAD_FOLDER + person.getId(), person.toJson());
             return true;
         } catch (Exception e) {
             log.error(e.getMessage());
             return false;
         }
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean delete(@PathVariable int id) {
+        if (this.s3client.doesObjectExist(this.awsS3Bucket, OBJECT_UPLOAD_FOLDER + id)) {
+            this.s3client.deleteObject(this.awsS3Bucket, OBJECT_UPLOAD_FOLDER + id);
+            return true;
+        }
+        return false;
     }
 }
--- aws-s3-upload/src/main/java/com/fd/aws/s3/model/Person.java
+++ aws-s3-upload/src/main/java/com/fd/aws/s3/model/Person.java
@@ -1,9 +1,14 @@
 package com.fd.aws.s3.model;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * @author fdanismaz
@@ -15,6 +20,22 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Person {
 
+    private int id;
     private String name;
     private String surname;
+
+    public String toJson() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(this);
+    }
+
+    public static Person fromJson(String text) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(text, Person.class);
+    }
+
+    public static Person fromJson(InputStream stream) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(stream, Person.class);
+    }
 }
